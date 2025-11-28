2253147c870121d39db44a4b4c5fba7b04b32184
Konosuke Sato <konosuke.s0912@gmail.com>
2025-11-28 07:33:24 +0000

    feat: options.luaに設定追加

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.swapfile = false
+vim.opt.autoread = true
+vim.opt.mouse = "a"
+
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.showmatch = true
+vim.opt.showcmd = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.fenc = "utf-8"
+
+vim.opt.undolevels = 1000
+
